b7d49bd1e9bab63e8f45b5ca67a41f85116a8dad
Chakru <abhishek.syncro@gmail.com>
2021-06-29 20:54:14 +0530

    Updated firebase key

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey:process.env.REACT_APP_APIKEY,
+  authDomain:process.env.REACT_APP_AUTHDOMAIN,
+  projectId:process.env.REACT_APP_PROJECTID,
+  storageBucket:process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID,
+  appId:process.env.REACT_APP_APPID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+export { db, auth };
